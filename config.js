b33228e4c420b8201504a857ceb22086cd5cb4c8
AX9T-hack <oulaisales@gmail.com>
2025-07-24 16:50:22 +0000

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ module.exports = {
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
     // ===== OWNER & DEVELOPER SETTINGS =====
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "50948702213",  // Owner's WhatsApp number
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "2250586358598",  // Owner's WhatsApp number
     
-    OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "",           // Owner's name
+    OWNER_NAME: process.env.OWNER_NAME || getConfig("Chris_MD") || "",           // Owner's name
     
     DEV: process.env.DEV || "50948336180",                     // Developer's contact number
     
